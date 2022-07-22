308f98d31f5c8c3ae0e5e84e079b684d75c4138c
신성은 <ibsun1215@gmail.com>
2022-07-22 16:46:15 +0900

    init: 프로젝트 초기 설정 2차 수정

---
+++ cogether-be/src/test/java/com/cogether/api/CogetherBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cogether.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CogetherBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
