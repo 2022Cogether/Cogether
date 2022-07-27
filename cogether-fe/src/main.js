065d6d25646290abb5eb791b5a35357b6ee23368
김진회 <kku64r@gmail.com>
2022-07-27 15:39:19 +0900

    (S07P12A801-390) feat:챌린지 방 레이아웃 기본 구현

---
+++ cogether-fe/src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+createApp(App).use(store).use(router).mount("#app");
+library.add(faUserSecret);
+App.component("font-awesome-icon", FontAwesomeIcon);
