d87720b5b361f60d2fff761d16cfe99cb80f9649
SSAFY <naryung92@naver.com>
2022-08-02 11:28:05 +0900

    (S07P12A801-222, S07P12A801-417) feat: 경쟁 모드 생성, 조회, 시간 추가 기능 구현

--- cogether-be/src/main/java/com/cogether/api/liveComp/domain/LiveCompResponse.java
+++ cogether-be/src/main/java/com/cogether/api/liveComp/domain/LiveCompResponse.java
@@ -1,5 +1,48 @@
 package com.cogether.api.liveComp.domain;
 
+import com.cogether.api.hunting.domain.HuntingResponse;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class LiveCompResponse {
+
+    @Getter
+    @Builder
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class OnlyId {
+        private long id;
+
+        public static LiveCompResponse.OnlyId build(LiveComp liveComp) {
+            return LiveCompResponse.OnlyId.builder()
+                    .id(liveComp.getId())
+                    .build();
+        }
+    }
+
+
+    @Getter
+    @Builder
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class GetLiveComp {
+        private int id;
+        private int userId;
+        private LocalTime totalTime;
+        private int ranking;
+        private int cnt;
+
+        public static LiveCompResponse.GetLiveComp build(LiveComp liveComp, int ranking, int cnt) {
+            return GetLiveComp.builder()
+                    .id(liveComp.getId())
+                    .userId(liveComp.getUser().getId())
+                    .totalTime(liveComp.getTotalTime())
+                    .ranking(ranking)
+                    .cnt(cnt)
+                    .build();
+        }
+    }
 
 }
--- cogether-be/src/main/java/com/cogether/api/liveComp/web/LiveCompController.java
+++ cogether-be/src/main/java/com/cogether/api/liveComp/web/LiveCompController.java
@@ -1,9 +1,16 @@
 package com.cogether.api.liveComp.web;
 
+import com.cogether.api.hunting.domain.HuntingRequest;
+import com.cogether.api.hunting.domain.HuntingResponse;
+import com.cogether.api.liveComp.domain.LiveCompRequest;
+import com.cogether.api.liveComp.domain.LiveCompResponse;
 import com.cogether.api.liveComp.service.LiveCompService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,5 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class LiveCompController {
 
     private final LiveCompService liveCompService;
+
+    @PutMapping("/liveComp")
+    @ResponseStatus(value = HttpStatus.OK)
+    public ResponseEntity<LiveCompResponse.OnlyId> update(@RequestBody LiveCompRequest.Update request) {
+        LiveCompResponse.OnlyId response = liveCompService.update(request);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("/liveComp/{userId}")
+    public ResponseEntity<LiveCompResponse.GetLiveComp> getLiveComp(@PathVariable int userId) {
+        LiveCompResponse.GetLiveComp response = liveCompService.getLiveComp(userId);
+        return ResponseEntity.ok().body(response);
+    }
 
 }
