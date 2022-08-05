64f27a5c3b83b1d677de1c678d2853aabbc958ec
신성은 <ibsun1215@gmail.com>
2022-08-05 17:32:04 +0900

    Project 조회 추가
    Service 상세조회 추가

--- cogether-be/src/main/java/com/cogether/api/project/service/ProjectService.java
+++ cogether-be/src/main/java/com/cogether/api/project/service/ProjectService.java
@@ -6,6 +6,7 @@ import com.cogether.api.project.domain.ProjectResponse;
 import com.cogether.api.project.domain.ProjectSkill;
 import com.cogether.api.project.exception.ProjectNotFoundException;
 import com.cogether.api.project.repository.ProjectRepository;
+import com.cogether.api.project.repository.ProjectScrapRepository;
 import com.cogether.api.project.repository.ProjectSkillRepository;
 import com.cogether.api.user.dto.User;
 import com.cogether.api.user.exception.UserNotFoundException;
@@ -13,7 +14,6 @@ import com.cogether.api.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -22,6 +22,7 @@ public class ProjectService {
 
     private final ProjectRepository projectRepository;
     private final ProjectSkillRepository projectSkillRepository;
+    private final ProjectScrapRepository projectScrapRepository;
     private final UserRepository userRepository;
 
     public ProjectResponse.OnlyId create(ProjectRequest.Create_Project create_project){
@@ -49,5 +50,17 @@ public class ProjectService {
         }
         projectRepository.deleteById(projectId);
         return ProjectResponse.OnlyId.build(project);
+    }
+
+    public ProjectResponse.ProjectAll getProjectDetail(int projectId, int userId){
+        Project project = projectRepository.findById(projectId).orElseThrow(ProjectNotFoundException::new);
+        User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+        List<ProjectSkill> list = projectSkillRepository.findAllByProject_Id(projectId);
+        int check = projectScrapRepository.countAllByProjectAndUser(project, user);
+        boolean isScrap = false;
+        if(check == 1){
+            isScrap = true;
+        }
+        return ProjectResponse.ProjectAll.build(project, list, isScrap);
     }
 }
