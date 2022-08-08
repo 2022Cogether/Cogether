b683f255557ead728c6573d46edb2be0de54a261
신성은 <ibsun1215@gmail.com>
2022-08-08 11:12:35 +0900

    (S07P12A801-420)feat: TIL 메인피드 조회 기능 구현
    Repository에서 userid로 til리스트 받는 jpa 추가

--- cogether-be/src/main/java/com/cogether/api/til/repository/TilRepository.java
+++ cogether-be/src/main/java/com/cogether/api/til/repository/TilRepository.java
@@ -10,4 +10,5 @@ public interface TilRepository extends JpaRepository<Til, Integer> {
     List<Til> findAllByContentContainingIgnoreCaseOrTitleContainingIgnoreCaseOrUser_NicknameContainingIgnoreCaseOrderByCreatedAtDesc(String keyword, String keyword2, String keyword3);
     List<Til> findAllByUserOrderByCreatedAtDesc(User user);
     List<Til> findAllByTitleContainingIgnoreCaseAndUserOrderByCreatedAtDesc(String keyword, User user);
+    List<Til> findAllByUser_Id (int userId);
 }
