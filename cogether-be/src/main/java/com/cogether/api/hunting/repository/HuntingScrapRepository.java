ae6e1254275eeeb7a4c972430ddb91363bd75fce
SSAFY <naryung92@naver.com>
2022-08-08 17:49:33 +0900

    feat : 구팀 삭제 기능 수정
    - 헌팅으로 헌팅스크랩 목록 조회하는 함수(findAllByHunting) 작성

--- cogether-be/src/main/java/com/cogether/api/hunting/repository/HuntingScrapRepository.java
+++ cogether-be/src/main/java/com/cogether/api/hunting/repository/HuntingScrapRepository.java
@@ -5,8 +5,12 @@ import com.cogether.api.hunting.domain.HuntingScrap;
 import com.cogether.api.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HuntingScrapRepository extends JpaRepository<HuntingScrap, Integer> {
     int countAllByUserAndHunting(User user, Hunting hunting);
 
     HuntingScrap findByUserAndHunting(User user, Hunting hunting);
+
+    List<HuntingScrap> findAllByHunting(Hunting hunting);
 }
