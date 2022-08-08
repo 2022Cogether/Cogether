5a38f54cea1e6ad95a5502fe6563991071e86fce
신성은 <ibsun1215@gmail.com>
2022-08-08 14:19:44 +0900

    (S07P12A801-178) feat: 스터디 구인 등록 기능 구현
    StudyScrapRepository JPA 파일 생성

---
+++ cogether-be/src/main/java/com/cogether/api/study/repository/StudyScrapRepository.java
@@ -0,0 +1,7 @@
+package com.cogether.api.study.repository;
+
+import com.cogether.api.study.domain.StudyScrap;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudyScrapRepository extends JpaRepository<StudyScrap, Integer> {
+}
