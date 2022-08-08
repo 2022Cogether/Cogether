2128510ecb4f2baf131418230e1e68035edebddb
신성은 <ibsun1215@gmail.com>
2022-08-08 15:50:28 +0900

    (S07P12A801-198) feat: 스터디 구인 상세보기 조회 기능 구현
    StudyScrapRepository에 Study와 User로 스크랩 여부 확인하는 JPA 추가

--- cogether-be/src/main/java/com/cogether/api/study/repository/StudyScrapRepository.java
+++ cogether-be/src/main/java/com/cogether/api/study/repository/StudyScrapRepository.java
@@ -1,7 +1,11 @@
 package com.cogether.api.study.repository;
 
+import com.cogether.api.study.domain.Study;
 import com.cogether.api.study.domain.StudyScrap;
+import com.cogether.api.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudyScrapRepository extends JpaRepository<StudyScrap, Integer> {
+
+    Integer countAllByStudyAndUser (Study study, User user);
 }
