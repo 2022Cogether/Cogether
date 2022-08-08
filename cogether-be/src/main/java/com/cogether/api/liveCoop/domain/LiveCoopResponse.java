1b350042839ba63f25f57ec1e5cfdd9ec971783a
SSAFY <naryung92@naver.com>
2022-08-08 11:08:54 +0900

    update : 함수 수정
    OnlyId에서 OnlyLiveCoopId로 함수명 수정,
    전체 협력모드방 조회에서 enterCoop(현재 참여중인지 여부) 변수 추가

--- cogether-be/src/main/java/com/cogether/api/liveCoop/domain/LiveCoopResponse.java
+++ cogether-be/src/main/java/com/cogether/api/liveCoop/domain/LiveCoopResponse.java
@@ -6,17 +6,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class LiveCoopResponse {
-
     @Getter
     @Builder
     @NoArgsConstructor(access = AccessLevel.PRIVATE)
     @AllArgsConstructor(access = AccessLevel.PRIVATE)
-    public static class OnlyId {
-        private int id;
+    public static class OnlyLiveCoopId {
+        private int liveCoopId;
 
-        public static LiveCoopResponse.OnlyId build(LiveCoop liveCoop) {
-            return LiveCoopResponse.OnlyId.builder()
-                    .id(liveCoop.getId())
+        public static LiveCoopResponse.OnlyLiveCoopId build(LiveCoop liveCoop) {
+            return OnlyLiveCoopId.builder()
+                    .liveCoopId(liveCoop.getId())
                     .build();
         }
     }
@@ -54,49 +53,21 @@ public class LiveCoopResponse {
         }
     }
 
-//    @Getter
-//    @Builder
-//    @NoArgsConstructor(access = AccessLevel.PRIVATE)
-//    @AllArgsConstructor(access = AccessLevel.PRIVATE)
-//    public static class GetLiveCoopInfo {
-//        private int id;
-//        private int userId;
-//        private String userName;
-//        private String userImgUrl;
-//        private int maxMemNum;
-//        private int nowMemNum;
-//        private int duration;
-//        private String title;
-//        private boolean inProgress;
-//
-//        public static GetLiveCoopInfo build(LiveCoop liveCoop) {
-//            return GetLiveCoopInfo.builder()
-//                    .id(liveCoop.getId())
-//                    .userId(liveCoop.getUser().getId())
-//                    .userName(liveCoop.getUser().getNickname())
-//                    .userImgUrl(liveCoop.getUser().getImgUrl())
-//                    .maxMemNum(liveCoop.getMemNum())
-//                    .nowMemNum(liveCoop.getNowMemNum())
-//                    .duration(liveCoop.getDuration())
-//                    .title(liveCoop.getTitle())
-//                    .inProgress(liveCoop.isInProgress())
-//                    .build();
-//        }
-//    }
-
     @Getter
     @Builder
     @NoArgsConstructor(access = AccessLevel.PRIVATE)
     @AllArgsConstructor(access = AccessLevel.PRIVATE)
     public static class GetLiveCoops {
         private List<GetLiveCoop> liveCoops;
+        private boolean enterCoop;
 
+        // TODO : , boolean isEnterCoop 수정 필요
         public static LiveCoopResponse.GetLiveCoops build(List<LiveCoop> liveCoops) {
             return LiveCoopResponse.GetLiveCoops.builder()
+                    .enterCoop(false)
                     .liveCoops(liveCoops.stream().map(LiveCoopResponse.GetLiveCoop::build).collect(Collectors.toList()))
                     .build();
         }
     }
-
 
 }
