ebd09628b728e3084e88180650f955c84c885359
신성은 <ibsun1215@gmail.com>
2022-08-10 15:56:34 +0900

    (S07P12A801-466, S07P12A801-467)feat: TIL 등록시 경험치, TILCNT 증가, TIL 삭제시 TILCNT 1 감소 기능 추가
    Repository에서 user로 해당 ranking찾는 JPA 추가

--- cogether-be/src/main/java/com/cogether/api/rank/respository/RankingRepository.java
+++ cogether-be/src/main/java/com/cogether/api/rank/respository/RankingRepository.java
@@ -1,7 +1,9 @@
 package com.cogether.api.rank.respository;
 
 import com.cogether.api.rank.domain.Ranking;
+import com.cogether.api.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RankingRepository extends JpaRepository<Ranking, Integer> {
+    Ranking findByUser(User user);
 }
