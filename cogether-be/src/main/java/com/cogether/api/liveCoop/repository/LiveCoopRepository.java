cf673fd5e99396a12f77e44af8ad5b88e856e074
SSAFY <naryung92@naver.com>
2022-08-11 14:18:01 +0900

    feat : 협력모드 목록 조회 기능 수정
    - findAllByOrderByCreatedAtDesc : 생성일 기준 역순으로 협력모드 목록 조회하는 함수
    - findAllByIdNotOrderByCreatedAtDesc : 생성일 기준 역순으로 id와 일치하지 않는 협력모드 목록 조회하는 함수
    - findByUser : 유저가 속한 협력모드 조회하는 함수

--- cogether-be/src/main/java/com/cogether/api/liveCoop/repository/LiveCoopRepository.java
+++ cogether-be/src/main/java/com/cogether/api/liveCoop/repository/LiveCoopRepository.java
@@ -1,8 +1,15 @@
 package com.cogether.api.liveCoop.repository;
 
 import com.cogether.api.liveCoop.domain.LiveCoop;
+import com.cogether.api.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LiveCoopRepository extends JpaRepository<LiveCoop, Integer> {
+import java.util.List;
 
+public interface LiveCoopRepository extends JpaRepository<LiveCoop, Integer> {
+    List<LiveCoop> findAllByOrderByCreatedAtDesc();
+
+    LiveCoop findByUser(User user);
+
+    List<LiveCoop> findAllByIdNotOrderByCreatedAtDesc(int id);
 }
