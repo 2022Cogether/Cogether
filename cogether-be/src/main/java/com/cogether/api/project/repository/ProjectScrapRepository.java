fc39d9d437e5f500f2db51975fa5cc845bb99891
신성은 <ibsun1215@gmail.com>
2022-08-14 12:37:39 +0900

    fix: projectId에 해당하는 projectscrap 리스트 반환하는 JPA 추가

--- cogether-be/src/main/java/com/cogether/api/project/repository/ProjectScrapRepository.java
+++ cogether-be/src/main/java/com/cogether/api/project/repository/ProjectScrapRepository.java
@@ -6,7 +6,10 @@ import com.cogether.api.project.domain.ProjectScrap;
 import com.cogether.api.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProjectScrapRepository extends JpaRepository<ProjectScrap, Integer> {
     Integer countAllByProjectAndUser(Project project, User user);
     ProjectScrap findByProject_IdAndUser_Id(int projectId, int userId);
+    List<ProjectScrap> findAllByProject_Id(int projectId);
 }
