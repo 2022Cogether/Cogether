247007ff183f7c38244beae9d48f661707bec659
신성은 <ibsun1215@gmail.com>
2022-08-14 14:55:14 +0900

    (S07P12A801-460) feat: til 랭킹 조회 기능 구현
    Repository에서 til수를 기준으로 랭킹 리스트 반환 JPA 추가

--- cogether-be/src/main/java/com/cogether/api/rank/respository/RankingRepository.java
+++ cogether-be/src/main/java/com/cogether/api/rank/respository/RankingRepository.java
@@ -4,7 +4,11 @@ import com.cogether.api.rank.domain.Ranking;
 import com.cogether.api.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RankingRepository extends JpaRepository<Ranking, Integer> {
     Ranking findByUser(User user);
     Ranking findByUser_Id(int userId);
+
+    List<Ranking> findAllByOrderByTilCntDesc();
 }
